f88c8ccd0c8f1ba0a9c9020e8d236066355b40e9
judgeOstrich <jeraldnyaga15@gmail.com>
2025-12-03 13:15:03 +0300

    Style the Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main(){
+    return <main className="main-comp">
+        <h2>About</h2>
+        <p>I am a front end developer with a keen eye in stellar looking designs that are mobile and desktop friendly. I love JS with its extended family of libraries and framework. We should definetly collaborate on your next project. Hit me up below on my handles. Cheers.</p>
+        <h2>Interest</h2>
+        <p>Coding, listening to Stan. Sleeping with my eyes open. Slow foods and non-interesting facts. Did you know you are about to know? Now you know</p>
+    </main>
+}
